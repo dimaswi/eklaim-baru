ba7d25feaea44b5694d917dd33326ebc1893d0fb
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-10-30 10:28:17 +0700

    Update DokterBPJS.php

--- app/Models/SIMRS/DokterBPJS.php
+++ app/Models/SIMRS/DokterBPJS.php
@@ -1,1 +1,18 @@
 
+<?php
+
+namespace App\Models\SIMRS;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DokterBPJS extends Model
+{
+    protected $connection = 'reg';
+
+    protected $table = 'dokter';
+
+    public function pegawai()
+    {
+        return $this->hasOne(Pegawai::class, 'NIP', 'NIP');
+    }
+}
