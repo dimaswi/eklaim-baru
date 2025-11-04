75e6dcc30063cfc525b8f16e42a117b6f3a11195
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-11-04 10:20:14 +0700

    Update PrintBundleController.php

--- app/Http/Controllers/Eklaim/PrintBundleController.php
+++ app/Http/Controllers/Eklaim/PrintBundleController.php
@@ -640,8 +640,6 @@ class PrintBundleController extends Controller
             
             // Get data based on document type
             $data = $this->getDocumentData($documentType, $pengajuanId, $selectedRecords);
-
-            dd($data);
             
             if (!$data || $data->isEmpty()) {
                 Log::warning('No data found for PDF', [
